e12d367d7944591652c0f129f1c094f7ceac2442
Gustavo-Alcantaara <gustavo.alcantaara@gmail.com>
2026-06-26 15:55:06 -0300

    finalizando app

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ class Bd{
                 continue;
             }
 
-
+            despesa.id = i
             despesas.push(despesa)
         }
 
@@ -107,6 +107,10 @@ class Bd{
 
         return despesasFiltradas
 
+        }
+
+        remover(id){
+            localStorage.removeItem(id)
         }
 }
 
@@ -205,9 +209,26 @@ function carregaListaDespesas(despesas = Array(), filtro = false){
             break 
         }
         linha.insertCell(1).innerHTML = d.tipo
-
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = `R$${d.valor}`
+
+        //criar botao de exclusão
+        let btn = document.createElement("button")
+        btn.className = 'btn btn-danger'
+        btn.innerHTML = '<i class="fas fa-times"></i>'
+        btn.id = `id_despesa_${d.id}`
+        btn.onclick = function(){
+            let id = this.id.replace('id_despesa_', '')
+            //alert(id)
+            
+            bd.remover(id)
+
+            window.location.reload()
+        }
+        linha.insertCell(4).append(btn)
+
+
+        console.log(d)
 
     })
 }
